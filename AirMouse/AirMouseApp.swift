84a731d6fa54cf408512f8a77b83a4d012a779c1
LightBlade12 <rehanjetha@gmail.com>
2025-11-15 12:17:36 -0500

    Initiated XCode Project as Multiplatform App

---
+++ AirMouse/AirMouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirMouseApp.swift
+//  AirMouse
+//
+//  Created by Rehan Jetha on 2025-11-15.
+//
+
+import SwiftUI
+
+@main
+struct AirMouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirMouseTests/AirMouseTests.swift
@@ -0,0 +1,16 @@
+//
+//  AirMouseTests.swift
+//  AirMouseTests
+//
+//  Created by Rehan Jetha on 2025-11-15.
+//
+
+import Testing
+
+struct AirMouseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
