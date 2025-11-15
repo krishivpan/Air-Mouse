bfb736ffdf7d818e464348b2ee0e8fd949a72127
LightBlade12 <rehanjetha@gmail.com>
2025-11-15 13:05:42 -0500

    Created XCode Project Files

---
+++ Project/Air Mouse Watch AppUITests/Air_Mouse_Watch_AppUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Air_Mouse_Watch_AppUITestsLaunchTests.swift
+//  Air Mouse Watch AppUITests
+//
+//  Created by Rehan Jetha on 2025-11-15.
+//
+
+import XCTest
+
+final class Air_Mouse_Watch_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
---
+++ Project/Air Mouse/Air_MouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Air_MouseApp.swift
+//  Air Mouse
+//
+//  Created by Rehan Jetha on 2025-11-15.
+//
+
+import SwiftUI
+
+@main
+struct Air_MouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
