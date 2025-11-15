2b0127f14c104acc34616de1d7d194f85a78c2a4
krishivpan <103297164+krishivpan@users.noreply.github.com>
2025-11-15 01:23:46 -0500

    Create skeleton for project

---
+++ Watch/Watch Watch App/WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchApp.swift
+//  Watch Watch App
+//
+//  Created by Krishiv Panchal on 2025-11-15.
+//
+
+import SwiftUI
+
+@main
+struct Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Watch/Watch/WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchApp.swift
+//  Watch
+//
+//  Created by Krishiv Panchal on 2025-11-15.
+//
+
+import SwiftUI
+
+@main
+struct WatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
