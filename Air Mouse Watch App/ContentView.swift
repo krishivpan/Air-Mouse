f633c4a34c60d8fa94fa809bb09ce24e900a75e8
LightBlade12 <rehanjetha@gmail.com>
2025-11-15 12:59:55 -0500

    Created Multiplatform + WatchOS App in XCode

---
+++ Air Mouse Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Air Mouse Watch App
+//
+//  Created by Rehan Jetha on 2025-11-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Air MouseTests/Air_MouseTests.swift
@@ -0,0 +1,16 @@
+//
+//  Air_MouseTests.swift
+//  Air MouseTests
+//
+//  Created by Rehan Jetha on 2025-11-15.
+//
+
+import Testing
+
+struct Air_MouseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
