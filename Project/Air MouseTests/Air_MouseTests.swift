d12ecef6bdef7136cfd475e86824db7a54ff9e37
LightBlade12 <rehanjetha@gmail.com>
2025-11-15 13:19:46 -0500

    Created WatchOS + iOS App

---
+++ Project/Air MouseTests/Air_MouseTests.swift
@@ -0,0 +1,17 @@
+//
+//  Air_MouseTests.swift
+//  Air MouseTests
+//
+//  Created by Rehan Jetha on 2025-11-15.
+//
+
+import Testing
+@testable import Air_Mouse
+
+struct Air_MouseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
