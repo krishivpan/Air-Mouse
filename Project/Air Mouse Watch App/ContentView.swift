b5222a3ed6d33ef38994d6c3878a357952eb28bb
krishivpan <103297164+krishivpan@users.noreply.github.com>
2025-11-15 14:40:25 -0500

    Started Watch App UI

--- Project/Air Mouse Watch App/ContentView.swift
+++ Project/Air Mouse Watch App/ContentView.swift
@@ -7,13 +7,23 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    @State private var test = "Hello Watch"
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("sdaf!")
+            
+            Button(test) {
+                print("Hello")
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(Color("Accent"))
         }
         .padding()
     }
