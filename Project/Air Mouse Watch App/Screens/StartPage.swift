c4b37e6bca7e1ff11fec5e37ea25bc33b39e99ef
LightBlade12 <rehanjetha@gmail.com>
2025-11-15 17:45:43 -0500

    Updated Connectivity Message

--- Project/Air Mouse Watch App/Screens/StartPage.swift
+++ Project/Air Mouse Watch App/Screens/StartPage.swift
@@ -8,22 +8,29 @@
 import SwiftUI
 
 struct StartPage: View {
+    @StateObject private var watchSession = WatchSessionManager.shared
+
     var body: some View {
         VStack(alignment: .leading, spacing: 4) {
             VStack {
                 Text("Gestures")
                     .font(.title2)
                     .foregroundColor(Color("Text"))
-                
-                Text("Connected to Mac")
-                    .font(.footnote)
-                    .foregroundColor(Color("Green"))
-                
+                if watchSession.isReachable {
+                    Text("Connected to iPhone")
+                        .font(.footnote)
+                        .foregroundColor(Color("Green"))
+                } else {
+                    Text("Not Connected to iPhone")
+                        .font(.footnote)
+                        .foregroundColor(Color("Red"))
+                }
+
                 Spacer().frame(height: 20) // Spacing before buttons
             }
             
             HStack {
-                GestureButton(title: "Click Me", action: { print("Success!") })
+                GestureButton(title: "Click Me", action: {watchSession.sendGesture(.tap) })
             }
         }
         .frame(maxWidth: .infinity, alignment: .leading)
