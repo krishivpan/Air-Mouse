0dbbc4098edd4f28964f854fb123a83f175859eb
LightBlade12 <rehanjetha@gmail.com>
2025-11-15 12:44:28 -0500

    Created the WatchOS App

---
+++ Air Mouse/Air Mouse Watch App/Air_MouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Air_MouseApp.swift
+//  Air Mouse Watch App
+//
+//  Created by Rehan Jetha on 2025-11-15.
+//
+
+import SwiftUI
+
+@main
+struct Air_Mouse_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
