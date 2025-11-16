6496d666fc895395854f61c68acf871870ec88a1
LightBlade12 <rehanjetha@gmail.com>
2025-11-16 00:28:21 -0500

    Added MacOS Connection and revised iOS connection

---
+++ Project/Air Mouse Mac App/Air_Mouse_Mac_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Air_Mouse_Mac_AppApp.swift
+//  Air Mouse Mac App
+//
+//  Created by Rehan Jetha on 2025-11-16.
+//
+
+import SwiftUI
+
+@main
+struct Air_Mouse_Mac_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
